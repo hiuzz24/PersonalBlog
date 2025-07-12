f00209170b1a637327bed93a37284b60bd73e4b7
hiuzz24 <hieupthe181180@fpt.edu.vn>
2025-07-12 20:56:01 +0700

    init

---
+++ src/test/java/com/he181180/personalblog/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.he181180.personalblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
