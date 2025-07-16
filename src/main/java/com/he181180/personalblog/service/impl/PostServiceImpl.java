b4f5cf53696f0b8168b3d5d4965188ac4840c371
MinMin0910 <minhkaka684@gmail.com>
2025-07-17 00:20:39 +0700

    1

--- src/main/java/com/he181180/personalblog/service/impl/PostServiceImpl.java
+++ src/main/java/com/he181180/personalblog/service/impl/PostServiceImpl.java
@@ -30,6 +30,15 @@ public class PostServiceImpl implements PostService {
     public List<Posts> findTop5RecentPosts() {
         return postRepository.findTop5ByOrderByPublishedAtDesc();
     }
+    @Override
+    public List<Posts> getPaginatedPosts(int page, int size) {
+        int start = (page - 1) * size;
+        return postRepository.findPostsWithPagination(start, size);
+    }
+
+    public int getTotalPostCount() {
+        return (int) postRepository.count(); // hoặc dùng custom SQL
+    }
 
     @Override
     public List<Posts> findPostsByTagID(int tagID) {
